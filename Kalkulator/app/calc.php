b7e592449790a0d3c3c8cbcb7dcb597d648c46ee
Yloru <100190883+Yloru@users.noreply.github.com>
2022-03-29 08:49:23 +0200

    Update calc.php

--- Kalkulator/app/calc.php
+++ Kalkulator/app/calc.php
@@ -1,26 +1,18 @@
 <?php
-// KONTROLER strony kalkulatora
 require_once dirname(__FILE__).'/../config.php';
-
-// W kontrolerze niczego nie wysyła się do klienta.
-// Wysłaniem odpowiedzi zajmie się odpowiedni widok.
-// Parametry do widoku przekazujemy przez zmienne.
-
 // 1. pobranie parametrów
 
 $N = $_REQUEST ['N'];
 $n = $_REQUEST ['n'];
 $p = $_REQUEST ['p'];
 
-// 2. walidacja parametrów z przygotowaniem zmiennych dla widoku
-
-// sprawdzenie, czy parametry zostały przekazane
+// 2. sprawdzenie, czy parametry zostały przekazane
 if ( ! (isset($N) && isset($n) && isset($p))) {
 	//sytuacja wystąpi kiedy np. kontroler zostanie wywołany bezpośrednio - nie z formularza
 	$messages [] = 'Błędne wywołanie aplikacji. Brak jednego z parametrów.';
 }
 
-// sprawdzenie, czy potrzebne wartości zostały przekazane
+// 3. sprawdzenie, czy potrzebne wartości zostały przekazane
 if ( $N == "") {
 	$messages [] = 'Nie podano kredytu.';
 }
@@ -30,11 +22,9 @@ if ( $n == "") {
 if ( $p == "") {
 	$messages [] = 'Nie podano oprocentowania.';
 }
- 
-//nie ma sensu walidować dalej gdy brak parametrów
-if (empty( $messages )) {
-	
-	// sprawdzenie wartości czy są liczbami
+
+
+// 4. sprawdzenie wartości czy są liczbami
 	if (! is_numeric( $N )) {
 		$messages [] = 'Wartość kredytu nie jest liczbą.';
 	}
@@ -46,11 +36,9 @@ if (empty( $messages )) {
 	if (! is_numeric( $p )) {
 		$messages [] = 'Wartość oprocentowania nie jest liczbą.';
 	}
-
 }
 
-// 3. wykonaj zadanie jeśli wszystko w porządku
-
+// 5. gdy jest brak bledow
 if (empty ( $messages )) { // gdy brak błędów
 	
 	//konwersja parametrów na float
@@ -61,7 +49,4 @@ if (empty ( $messages )) { // gdy brak błędów
 	//wykonanie operacji z zaokrągleniem do dwóch miejsc po przecinku (funkcja round).
 	$result = round((($N * $p * ($n+1))/2400),2);
 }
-
-// 4. Wywołanie widoku z przekazaniem zmiennych
-
 include 'calc_view.php';
