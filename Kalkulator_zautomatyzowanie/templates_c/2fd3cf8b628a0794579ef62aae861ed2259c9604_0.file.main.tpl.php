9123191639d5347a94da7f9142b30cf60aa25e65
Yloru <100190883+Yloru@users.noreply.github.com>
2022-04-12 00:07:05 +0200

    Add files via upload

---
+++ Kalkulator_zautomatyzowanie/templates_c/2fd3cf8b628a0794579ef62aae861ed2259c9604_0.file.main.tpl.php
@@ -0,0 +1,55 @@
+<?php
+/* Smarty version 3.1.30, created on 2022-04-11 23:34:28
+  from "C:\xampp\htdocs\Kalkulator\app\views\templates\main.tpl" */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.30',
+  'unifunc' => 'content_62549ee4a6eab3_51954681',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '2fd3cf8b628a0794579ef62aae861ed2259c9604' => 
+    array (
+      0 => 'C:\\xampp\\htdocs\\Kalkulator\\app\\views\\templates\\main.tpl',
+      1 => 1523565760,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_62549ee4a6eab3_51954681 (Smarty_Internal_Template $_smarty_tpl) {
+$_smarty_tpl->_loadInheritance();
+$_smarty_tpl->inheritance->init($_smarty_tpl, false);
+?>
+<!DOCTYPE HTML>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="pl" lang="pl">
+<head>
+	<meta charset="utf-8" />
+	<title><?php echo (($tmp = @$_smarty_tpl->tpl_vars['page_title']->value)===null||$tmp==='' ? "brak tytułu" : $tmp);?>
+</title>
+	<link rel="stylesheet" href="https://unpkg.com/purecss@0.6.2/build/pure-min.css" integrity="sha384-UQiGfs9ICog+LwheBSRCt1o5cbyKIHbwjWscjemyBMT9YCUMZffs6UqUTd0hObXD" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="<?php echo $_smarty_tpl->tpl_vars['conf']->value->app_url;?>
+/css/style.css" />
+</head>
+<body>
+	<div style="margin: 1em;">
+		<?php 
+$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_191737499662549ee4a6e520_39621019', 'content');
+?>
+
+	</div>
+</body>
+</html><?php }
+/* {block 'content'} */
+class Block_191737499662549ee4a6e520_39621019 extends Smarty_Internal_Block
+{
+public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
+?>
+ Domyślna treść zawartości .... <?php
+}
+}
+/* {/block 'content'} */
+}
---
+++ Kalkulator_zautomatyzowanie/templates_c/d82cdbbbb8d943cba2104b58174a57c8fc133791_0.file.messages.tpl.php
@@ -0,0 +1,63 @@
+<?php
+/* Smarty version 3.1.30, created on 2022-04-11 23:34:28
+  from "C:\xampp\htdocs\Kalkulator\app\views\templates\messages.tpl" */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.30',
+  'unifunc' => 'content_62549ee4bdb5b3_03769668',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    'd82cdbbbb8d943cba2104b58174a57c8fc133791' => 
+    array (
+      0 => 'C:\\xampp\\htdocs\\Kalkulator\\app\\views\\templates\\messages.tpl',
+      1 => 1523615504,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_62549ee4bdb5b3_03769668 (Smarty_Internal_Template $_smarty_tpl) {
+if ($_smarty_tpl->tpl_vars['msgs']->value->isError()) {?>
+<div class="messages error">
+	<ol>
+	<?php
+$_from = $_smarty_tpl->smarty->ext->_foreach->init($_smarty_tpl, $_smarty_tpl->tpl_vars['msgs']->value->getErrors(), 'err');
+if ($_from !== null) {
+foreach ($_from as $_smarty_tpl->tpl_vars['err']->value) {
+?>
+	<li><?php echo $_smarty_tpl->tpl_vars['err']->value;?>
+</li>
+	<?php
+}
+}
+$_smarty_tpl->smarty->ext->_foreach->restore($_smarty_tpl);
+?>
+
+	</ol>
+</div>
+<?php }
+if ($_smarty_tpl->tpl_vars['msgs']->value->isInfo()) {?>
+<div class="messages info bottom-margin">
+	<ol>
+	<?php
+$_from = $_smarty_tpl->smarty->ext->_foreach->init($_smarty_tpl, $_smarty_tpl->tpl_vars['msgs']->value->getInfos(), 'inf');
+if ($_from !== null) {
+foreach ($_from as $_smarty_tpl->tpl_vars['inf']->value) {
+?>
+	<li><?php echo $_smarty_tpl->tpl_vars['inf']->value;?>
+</li>
+	<?php
+}
+}
+$_smarty_tpl->smarty->ext->_foreach->restore($_smarty_tpl);
+?>
+
+	</ol>
+</div>
+<?php }
+}
+}
